6727b6313e937a6e640900083f468c55e55b9fc8
Kola Kolade <kolakolade@gmail.com>
2023-01-18 13:39:32 +0100

    Add html markup and function to add book

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+const myLibrary = [],
+addBookBtn = document.querySelector('#add-book-button');
+
 function Book(title, author, pages, isRead){
   this.title = title
   this.author = author
@@ -7,4 +10,30 @@ function Book(title, author, pages, isRead){
 
 Book.prototype.info = function (){
   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead ? 'read':'not read yet'}`;
+}
+
+const addBookToLibrary = (e) => {
+  e.preventDefault();
+
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let pages = document.querySelector('#pages').value;
+  let isRead = document.querySelector('#is-read').checked;
+
+  const book = new Book(title, author, pages, isRead);
+  
+  myLibrary.push(book);
+
+  // title.value = '';
+  // author.value = '';
+  // pages.value = '';
+  // isRead.checked = false
+}
+
+addBookBtn.addEventListener('click', addBookToLibrary)
+
+const displayBook = () => {
+  myLibrary.forEach(book => {
+
+  })
 }
