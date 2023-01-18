5f4f6970cc6c5e1416ddc8b60a5849ff16102a6a
Kola Kolade <kolakolade@gmail.com>
2023-01-18 12:02:26 +0100

    Add Book constructor

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function Book(title, author, pages, isRead){
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.isRead = isRead
+}
+
+Book.prototype.info = function (){
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead ? 'read':'not read yet'}`;
+}
